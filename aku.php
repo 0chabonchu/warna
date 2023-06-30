5269e13bd9228c4604143c8a0388e95f301522f7
0chabonchu <138196179+0chabonchu@users.noreply.github.com>
2023-06-30 17:08:49 +0700

    Create aku.php

---
+++ aku.php
@@ -0,0 +1,47 @@
+<?php
+
+class Colors {
+    private $foreground_colors = array();
+
+    public function __construct() {
+        $this->foreground_colors['black'] = '0;30';
+        $this->foreground_colors['dark_gray'] = '1;30';
+        $this->foreground_colors['blue'] = '0;34';
+        $this->foreground_colors['light_blue'] = '1;34';
+        $this->foreground_colors['green'] = '0;32';
+        $this->foreground_colors['light_green'] = '1;32';
+        $this->foreground_colors['cyan'] = '0;36';
+        $this->foreground_colors['light_cyan'] = '1;36';
+        $this->foreground_colors['red'] = '0;31';
+        $this->foreground_colors['light_red'] = '1;31';
+        $this->foreground_colors['purple'] = '0;35';
+        $this->foreground_colors['light_purple'] = '1;35';
+        $this->foreground_colors['brown'] = '0;33';
+        $this->foreground_colors['yellow'] = '1;33';
+        $this->foreground_colors['light_gray'] = '0;37';
+        $this->foreground_colors['white'] = '1;37';
+    }
+
+    // Returns colored string
+    public function getColoredString($string, $foreground_color = null) {
+        $colored_string = "";
+
+        if (isset($this->foreground_colors[$foreground_color])) {
+            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+        }
+
+        $colored_string .=  $string . "\033[0m";
+
+        return $colored_string;
+    }
+
+    // Returns all foreground color names
+    public function getForegroundColors() {
+        return array_keys($this->foreground_colors);
+    }
+}
+
+$colors = new Colors();
+
+echo $colors->getColoredString("Test String", "blue");
+?>
